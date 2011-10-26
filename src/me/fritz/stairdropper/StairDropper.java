d1781da949ed40d86be277a4833598aac65f9631
fritz <fritzed@gmail.com>
2011-10-25 17:55:48 -0700

    Minor code changes to make netbeans happy

--- src/me/fritz/stairdropper/StairDropper.java
+++ src/me/fritz/stairdropper/StairDropper.java
@@ -1,9 +1,6 @@
 package me.fritz.stairdropper;
 
 import java.util.logging.Logger;
-
-import me.fritz.stairdropper.StairDropperBlockListener;
-
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,21 +12,23 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class StairDropper extends JavaPlugin{
 
-	private StairDropperBlockListener blockListener = new StairDropperBlockListener();
-	Logger log = Logger.getLogger("Minecraft");
+    private StairDropperBlockListener blockListener = new StairDropperBlockListener();
+    static final Logger log = Logger.getLogger("Minecraft");
 
-	/** Called when the plugin is enabled
-	 */
-	public void onEnable() {
-		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvent(Event.Type.BLOCK_BREAK, blockListener, Event.Priority.Normal, this);
-		
-		log.info("[StairDropper] plugin enabled.");
-	}
+    /** Called when the plugin is enabled
+     */
+    @Override
+    public void onEnable() {
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvent(Event.Type.BLOCK_BREAK, blockListener, Event.Priority.Normal, this);
 
-	/** Called when the plugin is disabled
-	 */
-	public void onDisable() {
-		log.info("[StairDropper] plugin disabled.");
-	}
+        log.info("[StairDropper] plugin enabled.");
+    }
+
+    /** Called when the plugin is disabled
+     */
+    @Override
+    public void onDisable() {
+        log.info("[StairDropper] plugin disabled.");
+    }
 }
